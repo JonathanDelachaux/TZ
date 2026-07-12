dd3bf4a5ef270a2e5941f066d67d0816ab8d8310
JonathanDelachaux <contact@jonathandelachaux.com>
2026-07-12 18:41:17 +0200

    visite(suite)

---
+++ tchan-zca(3)/app-files/data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9181578334528808,
+          "pitch": -1.1966991853276046,
+          "rotation": 0,
+          "target": "3-04"
+        },
+        {
+          "yaw": 1.7233598909340184,
+          "pitch": 0.012751783061062838,
+          "rotation": 0,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7601998235737444,
+          "pitch": -0.027551296750111476,
+          "rotation": 0,
+          "target": "0-01"
+        },
+        {
+          "yaw": 1.1264136854559261,
+          "pitch": 0.06982436489376553,
+          "rotation": 0,
+          "target": "2-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.985378096825107,
+          "pitch": -1.119167540748787,
+          "rotation": 0,
+          "target": "1-02"
+        },
+        {
+          "yaw": 1.785155875962035,
+          "pitch": 0.1933024094847191,
+          "rotation": 0,
+          "target": "3-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04",
+      "name": "04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6648810979827822,
+          "pitch": -1.3723663904880343,
+          "rotation": 0,
+          "target": "2-03"
+        },
+        {
+          "yaw": 0.26490285874757014,
+          "pitch": 0.873048045435624,
+          "rotation": 0,
+          "target": "0-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8130827412340196,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Tchan-Zâca",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
