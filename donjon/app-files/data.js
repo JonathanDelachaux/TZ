5f02dcf7b17a7ad5b4397863a7b1ef0ef05e7dc0
JonathanDelachaux <contact@jonathandelachaux.com>
2026-07-22 16:43:00 +0200

    donjon

---
+++ donjon/app-files/data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_web_vassiliok1",
+      "name": "360_web_vassiliok1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.476821272481839,
+          "pitch": 0.08151652824024502,
+          "rotation": 0,
+          "target": "1-360_web_vassiliok2"
+        },
+        {
+          "yaw": 2.2336680588372655,
+          "pitch": 0.06049040293128982,
+          "rotation": 0,
+          "target": "4-360_web_vassiliok6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360_web_vassiliok2",
+      "name": "360_web_vassiliok2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.698182676722479,
+          "pitch": -0.02914079709261408,
+          "rotation": 0,
+          "target": "0-360_web_vassiliok1"
+        },
+        {
+          "yaw": 0.9346581470398778,
+          "pitch": 0.32576951782621677,
+          "rotation": 0,
+          "target": "2-360_web_vassiliok3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360_web_vassiliok3",
+      "name": "360_web_vassiliok3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.104799658237491,
+          "pitch": -1.2927558490031803,
+          "rotation": 0,
+          "target": "1-360_web_vassiliok2"
+        },
+        {
+          "yaw": -0.630841553386416,
+          "pitch": 0.08065468304726409,
+          "rotation": 0,
+          "target": "3-360_web_vassiliok4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-360_web_vassiliok4",
+      "name": "360_web_vassiliok4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6010027302436045,
+          "pitch": -0.85706713280236,
+          "rotation": 0,
+          "target": "2-360_web_vassiliok3"
+        },
+        {
+          "yaw": 1.6777254869430873,
+          "pitch": -0.027304537538856977,
+          "rotation": 0,
+          "target": "4-360_web_vassiliok6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-360_web_vassiliok6",
+      "name": "360_web_vassiliok6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6466763129668909,
+          "pitch": -0.03542922743675092,
+          "rotation": 0,
+          "target": "3-360_web_vassiliok4"
+        },
+        {
+          "yaw": 2.3393251048469805,
+          "pitch": 0.1609189877693602,
+          "rotation": 0,
+          "target": "0-360_web_vassiliok1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
